1b0f444e9b3813f66dcd0f38bcd36db4a73cf317
Trisslotten <tristan23@live.se>
2018-10-31 16:31:12 +0100

    ran scalability test

---
+++ scalabilitytest.sh
@@ -0,0 +1,17 @@
+N=2
+Size=1000000
+for i in {1..5}
+do
+	echo size = $Size > scale1-${i}.txt
+	{ time ./a.out $Size 1 data10e9 test ; } 2>> scale1-${i}.txt
+	((Size= Size * N))
+done
+
+N=10
+Size=1
+for i in {1..5}
+do
+	echo buffersize = $Size > scale2-${i}.txt
+	{ time ./a.out 1000000 $Size data10e9 test ; } 2>> scale2-${i}.txt
+	((Size= Size * N))
+done
