5421ebd5428b1dfebe89797c150d5915d3dfbd63
Trisslotten <tristan23@live.se>
2018-10-31 17:23:52 +0100

    better parameters to scalability test

--- scalabilitytest.sh
+++ scalabilitytest.sh
@@ -12,6 +12,6 @@ Size=1
 for i in {1..5}
 do
 	echo buffersize = $Size > scale2-${i}.txt
-	{ time ./a.out 1000000 $Size data10e9 test ; } 2>> scale2-${i}.txt
+	{ time ./a.out 10000000 $Size data10e9 test ; } 2>> scale2-${i}.txt
 	((Size= Size * N))
 done
